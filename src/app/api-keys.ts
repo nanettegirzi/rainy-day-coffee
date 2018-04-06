93eecb7d9c644ceb7d0a852d680594a5310dfe77
Nanette Girzi <ngirzi@gmail.com>
2018-04-06 10:06:25 -0700

    install angualrfire and add api key file

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+//enter your Firebase credentials here
+
+export var masterFirebaseConfig = {
+    apiKey: "xxxx",
+    authDomain: "xxxx.firebaseapp.com",
+    databaseURL: "https://xxxx.firebaseio.com",
+    storageBucket: "xxxx.appspot.com",
+    messagingSenderId: "xxxx"
+  };
